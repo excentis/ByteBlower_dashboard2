ab2fe4cf51262b6f88d523c5bd55ca70835005d8
root <root@Debian-9.3-64Bit>
2018-05-09 16:33:15 +0200

    A lot more logging

--- byteblower_poller/poll.py
+++ byteblower_poller/poll.py
@@ -1,9 +1,35 @@
 import byteblowerll.byteblower as byteblower
+import datetime
 import requests
 import time
+import itertools
 
 WEBSERVER = 'http://10.1.1.17'
-class Updater:
+DEBUG_FILE = '/tmp/interop_traffic.log'
+
+class DebugLogger(object):
+    """
+        Batches several logs
+
+        @detail
+        Balancing between keeping things simple and
+         1. Logging centralized.
+         2. Some performance gains.
+    """
+    def __init__(self):
+        self.logs = []
+
+    def log(self, log_line):
+        self.logs.append(log_line)
+        if len(self.logs) > 1024:
+            with open(DEBUG_FILE, 'a') as f:
+                for line in self.logs:
+                    f.write(line + "\n")
+            self.logs = []                    
+
+
+logger = DebugLogger()
+class Updater(object):
     """
         A helper class to fetch the results from the ByteBlower servers
 
@@ -32,8 +58,9 @@ class Updater:
         [b.Clear() for b in self.update]
         [special_list.append(b) for b in self.update]
         api.ResultsRefresh(special_list)
-            
-class PortTrigger: 
+       
+port_trigger_count = 0       
+class PortTrigger(object): 
     """
         A single, virtual port counter.
         
@@ -46,6 +73,15 @@ class PortTrigger:
         self.trigger = self.port.RxTriggerBasicAdd()
         self.trigger.FilterSet(bpf)
 
+        self.server_name = bb_server.ServiceInfoGet().ConnectionHostGet()
+        self.bb_interface = bb_interface
+        self.detail = ''
+        self.bpf = bpf
+
+        global port_trigger_count
+        port_trigger_count += 1
+
+    
     def update(self, updater):
         updater.add(self.trigger.ResultHistoryGet())
     
@@ -63,18 +99,54 @@ class PortTrigger:
         sample_duration = last_interval.IntervalDurationGet() * 1e-9 # seconds
         bytes = last_interval.ByteCountGet()
         packets = last_interval.PacketCountGet()
+
+        min_framesize = 0 
+        max_framesize = 0 
+    
+        if packets > 0:
+            min_framesize = last_interval.FramesizeMinimumGet() 
+            max_framesize = last_interval.FramesizeMaximumGet() 
+
         frame_overhead = 24 # bytes
         bytes_to_bits = 8
         physical_count = (bytes + (frame_overhead * packets)) * bytes_to_bits 
 
+        global logger
+        logline = "%d, %s, %s, %s, %s, %d, %d, %d, %d" % (
+                last_interval.TimestampGet(),
+                self.detail,
+                self.server_name,
+                self.bb_interface,
+                self.bpf, 
+                bytes,
+                packets,
+                min_framesize,
+                max_framesize )
+                        
+        logger.log(logline)
         return physical_count / sample_duration
 
-class TriggerGroup: 
+class TriggerGroup(object): 
     """
         A group of port triggers. Responsible for aggregating several results.        
     """
     def __init__(self, *port_triggers):
         self.triggers = port_triggers
+        self._detail = ''
+
+    @property 
+    def detail(self):
+        return self._detail
+
+    @detail.setter
+    def detail(self,value):
+        self._detail = value
+        for trig in self.triggers:
+            trig.detail = value
+
+    @detail.deleter
+    def detail(self):
+        del self._detail
 
     @staticmethod
     def combinatorial(bb_servers, interfaces, bpf_filters = ['']):
@@ -94,7 +166,6 @@ class TriggerGroup:
                 for bpf in bpf_filters: 
                     port = PortTrigger(server, interface, bpf)
                     port_list.append(port)
-        print(port_list)                    
         return TriggerGroup(*port_list)                    
 
     def update(self, updater):
@@ -104,16 +175,21 @@ class TriggerGroup:
         return sum([trig.result()  for trig in self.triggers])
 
 
-class Vendor:
+class Vendor(object):
     """
         Abstracts a single vendor.
 
         Each vendor has single (aggregated) upstream and downstream.
     """
-    def __init__(self, name, upstream, downstream):
+    def __init__(self, name, upstream, downstream, debug= False):
         self.name = name
         self.upstream = upstream
         self.downstream = downstream
+
+        upstream.detail = "up " + name
+        downstream.detail = "down " + name
+
+        self.debug_trigger = debug
 
     def update(self, updater):
         self.upstream.update(updater)
@@ -124,8 +200,10 @@ class Vendor:
                 "downstream" : self.downstream.result() }
 
     def post_results(self, base_url):
-        specific = base_url + "/" + self.name
-        r = requests.post(specific, data = self.result())
+        result = self.result()
+        if not self.debug_trigger:
+            specific = base_url + "/" + self.name
+            r = requests.post(specific, data = result) 
 
 
 
@@ -137,7 +215,6 @@ def single_update(vendors):
     update.refresh(byteblower.ByteBlower.InstanceGet())
 
     for v in vendors:
-        print("%s, %s" % (v.name, str(v.result())))
         v.post_results(WEBSERVER)
 
 
@@ -233,18 +310,21 @@ vendors = [Vendor('intel',
                      ['ip dst 10.%d.254.19' % d for d in range(240,250)]),
                   downstream = TriggerGroup.combinatorial( 
                       [bb_cpe1],
-                      ['trunk-4-%d' % d for d in range(1,13)]))
+                      ['trunk-4-%d' % d for d in range(1,13)])),
+            Vendor('all-triggers', debug = True,
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe1, bb_cpe2],
+                      itertools.chain ( *[['trunk-%d-%d' % (interface, trunk) 
+                                                    for trunk in range(1,48)] 
+                                                    for interface in [1,2,3,4]]))
+                  )
             ]
-#vendors = [Vendor('intel',
-#                  upstream = TriggerGroup(
-#                     PortTrigger(SERVER, 'nontrunk-1', 'ip dst net 10.241.0.0 mask'), 
-#                     PortTrigger(SERVER, 'nontrunk-1', '') ), 
-#                  downstream = TriggerGroup(
-#                     PortTrigger(SERVER, 'nontrunk-1', '') ),
-#                  )
-#            ]                  
-#
+print("Has %d triggers" % port_trigger_count)
 while True:
+    print('update ' + str(datetime.datetime.now()))
     single_update(vendors)
     time.sleep(0.9)
 
