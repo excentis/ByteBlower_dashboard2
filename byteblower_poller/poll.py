097caa3f447310951036879728b273caf7d30fc8
root <root@Debian-9.3-64Bit>
2018-05-09 14:40:16 +0200

    Configured setup

--- byteblower_poller/poll.py
+++ byteblower_poller/poll.py
@@ -51,7 +51,7 @@ class PortTrigger:
     def result(self):
         """
             Current bandwidht on the interface.
-            Returned in bytes per second, includes frame overhead.
+            Returned in bits/s, includes frame overhead.
             Filling the line with 10 Gbit/s will display 10 Gbit/s
         """
         history = self.trigger.ResultHistoryGet()
@@ -63,7 +63,8 @@ class PortTrigger:
         bytes = last_interval.ByteCountGet()
         packets = last_interval.PacketCountGet()
         frame_overhead = 24 # bytes
-        physical_count = bytes + (frame_overhead * packets)
+        bytes_to_bits = 8
+        physical_count = (bytes + (frame_overhead * packets)) * bytes_to_bits 
 
         return physical_count / sample_duration
 
@@ -73,6 +74,27 @@ class TriggerGroup:
     """
     def __init__(self, *port_triggers):
         self.triggers = port_triggers
+
+    @staticmethod
+    def combinatorial(bb_servers, interfaces, bpf_filters = ['']):
+        """
+            A factory method to easily create a Group of lots of PortTriggers.
+
+            The group will match all in the filters on all of the interfaces on all
+            of the servers.
+
+            @arg bb_server a list of  byteblower servers
+            @arg interfaces a list of byteblower interfaces.
+            @arg bpf_filters a list of BPF filters. Default everything is matched.
+        """
+        port_list = []
+        for server in bb_servers:
+            for interface in interfaces:
+                for bpf in bpf_filters: 
+                    port = PortTrigger(server, interface, bpf)
+                    port_list.append(port)
+        print(port_list)                    
+        return TriggerGroup(*port_list)                    
 
     def update(self, updater):
         [trig.update(updater) for trig in self.triggers]
@@ -120,16 +142,114 @@ def single_update(vendors):
 
 # A DSL to configure the system
 API = byteblower.ByteBlower.InstanceGet()
-SERVER = API.ServerAdd("byteblower-dev-1300-2.lab.byteblower.excentis.com")
+def bb_server(address):
+    try:
+        return API.ServerAdd(address)
+    except:
+        print("Can't reach %s" % address)
 
+# The actual config
+SERVER = bb_server("byteblower-dev-1300-2.lab.byteblower.excentis.com")
+#SERVER = bb_server("byteblower-tutorial-1300.lab.byteblower.excentis.com")
+
+bb_cpe1 = bb_server("byteblower-iop-CPE1.interop.excentis.com")
+bb_cpe2 = bb_server("byteblower-iop-CPE2.interop.excentis.com")
+bb_nsi1 = bb_server("byteblower-iop-NSI1.interop.excentis.com")
+bb_nsi2 = bb_server("byteblower-iop-NSI2.interop.excentis.com")
+
+
+all_nontrunks = ['nontrunk-1', 'nontrunk-2', 'nontrunk-3', 'nontrunk-4']
+all_nsi_byteblowers = [bb_nsi1, bb_nsi2]
 vendors = [Vendor('intel',
-                  upstream = TriggerGroup(
-                     PortTrigger(SERVER, 'nontrunk-1', ''), 
-                     PortTrigger(SERVER, 'nontrunk-1', '') ), 
-                  downstream = TriggerGroup(
-                     PortTrigger(SERVER, 'nontrunk-1', '') ),
+                  upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.11' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe1],
+                      ['trunk-2-%d' % d for d in range(1,5)])),
+            Vendor('broadcom',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.12' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe1],
+                      ['trunk-2-%d' % d for d in range(5,9)])),
+            Vendor('sagemcom',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.13' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe2],
+                      ['trunk-3-%d' % d for d in range(1,13)])),
+            Vendor('hitron',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.14' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe2],
+                      ['trunk-1-%d' % d for d in range(1,13)])),
+            Vendor('technicolor',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.15' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe2],
+                      ['trunk-4-%d' % d for d in range(1,13)])),
+            Vendor('cbn',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.16' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe2],
+                      ['trunk-2-%d' % d for d in range(1,13)])),
+            Vendor('avm',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.17' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe1],
+                      ['trunk-1-%d' % d for d in range(1,13)])),
+            Vendor('askey',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.18' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.combinatorial( 
+                      [bb_cpe1],
+                      ['trunk-3-%d' % d for d in range(1,13)])),
+            Vendor('ubee',
+                upstream = TriggerGroup.combinatorial(
+                     all_nsi_byteblowers,
+                     all_nontrunks,
+                     ['ip dst 10.%d.254.19' % d for d in range(240,250)]),
+                  downstream = TriggerGroup.Combinatorial( 
+                      [bb_cpe1],
+                      ['trunk-4-%d' % d for d in range(1,13)]))
+            ]
+#vendors = [Vendor('intel',
+#                  upstream = TriggerGroup(
+#                     PortTrigger(SERVER, 'nontrunk-1', 'ip dst net 10.241.0.0 mask'), 
+#                     PortTrigger(SERVER, 'nontrunk-1', '') ), 
+#                  downstream = TriggerGroup(
+#                     PortTrigger(SERVER, 'nontrunk-1', '') ),
+#                  )
+#            ]                  
+#
+vendors = [Vendor('intel',
+                  upstream = TriggerGroup.combinatorial(
+                      [SERVER],
+                      ['nontrunk-1', 'nontrunk-2']),
+                  downstream = 
+                     PortTrigger(SERVER, 'nontrunk-1', '') ,
                   )
-            ]                  
+    ]  
 
 
 for _ in xrange(100):
